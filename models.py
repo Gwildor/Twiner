a0be5cea6392a7f1fea57cb54456963ffb20504c
Gwildor Sok <gwildorsok@gmail.com>
2013-07-21 01:32:54 +0200

    Add model for Tweets.

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import datetime
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, DateTime, Integer, Interval, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, Interval, String
 
 Base = declarative_base()
 
@@ -14,7 +15,22 @@ class Timeline(Base):
     interval = Column(Interval, default=datetime.timedelta(minutes=5))
     last_update = Column(DateTime)
 
+    tweets = relationship('Tweet', backref='timeline', lazy='dynamic')
+
     def __repr__(self):
         return '<Timeline({0}, {1}, {2})>'.format(self.screen_name,
                                                   self.interval,
                                                   self.last_update)
+
+
+class Tweet(Base):
+    __tablename__ = 'Twiner_tweets'
+
+    id = Column(Integer, primary_key=True)
+    timeline_id = Column(Integer, ForeignKey(Timeline.id))
+    text = Column(String(length=200))
+    date = Column(DateTime)
+
+    def __repr__(self):
+        return '<Tweet({0}, {1}, {2})>'.format(self.timeline.screen_name,
+                                               self.text, self.date)
