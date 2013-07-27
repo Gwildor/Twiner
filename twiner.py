0b73670dec7b68943635e2211fc3c09380d0080e
Gwildor Sok <gwildorsok@gmail.com>
2013-07-27 17:50:58 +0200

    Add basic code for fetching and saving new tweets.

--- twiner.py
+++ twiner.py
@@ -1,3 +1,4 @@
+import datetime
 import ConfigParser
 import io
 from sqlalchemy import create_engine
@@ -34,6 +35,29 @@ def run():
 
     api = Api(conf.get('api', 'consumer_key'), conf.get('api',
                                                         'consumer_secret'))
+
+    params = {'count': 3, 'trim_user': 1, 'exclude_replies': 1,
+              'include_rts': 0}
+
+    for tl in session.query(models.Timeline):
+
+        if not tl.last_update or datetime.datetime.now() >= tl.last_update + tl.interval:
+
+            last_tweet = tl.tweets.order_by(models.Tweet.id.desc()).first()
+            if last_tweet:
+                params['since_id'] = last_tweet.id
+            elif 'since_id' in params:
+                del params['since_id']
+
+            tweets = api.get_user_timeline(params=params,
+                                           screen_name=tl.screen_name)
+            for tweet in tweets:
+                if not session.query(models.Tweet).get(tweet['id']):
+                    tl.tweets.append(models.Tweet(tweet))
+
+            tl.last_update = datetime.datetime.now()
+
+    session.commit()
 
 if __name__ == '__main__':
     run()
