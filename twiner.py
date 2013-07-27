5fe417377b0cfc111c1841c1c0e5a7e333c5e08a
Gwildor Sok <gwildorsok@gmail.com>
2013-07-27 17:44:28 +0200

    Wrap main code in a function.

--- twiner.py
+++ twiner.py
@@ -6,28 +6,34 @@ from sqlalchemy.orm import sessionmaker
 from api import Api
 import models
 
-conf = ConfigParser.SafeConfigParser()
-with io.open('conf.ini', 'rb') as fd:
-    conf.readfp(fd)
 
-try:
-    db_url = conf.get('database', 'url')
-except ConfigParser.NoOptionError:
-    if conf.get('database', 'type') == 'sqlite':
-        db_url = 'sqlite://{0}'.format(conf.get('database', 'name'))
-    else:
-        db_url = '{0}://{1}:{2}@{3}/{4}'.format(
-            conf.get('database', 'type'),
-            conf.get('database', 'user'),
-            conf.get('database', 'password'),
-            conf.get('database', 'host'),
-            conf.get('database', 'name')
-        )
+def run():
+    conf = ConfigParser.SafeConfigParser()
+    with io.open('conf.ini', 'rb') as fd:
+        conf.readfp(fd)
 
-engine = create_engine(db_url)
-models.Base.metadata.create_all(bind=engine)
+    try:
+        db_url = conf.get('database', 'url')
+    except ConfigParser.NoOptionError:
+        if conf.get('database', 'type') == 'sqlite':
+            db_url = 'sqlite://{0}'.format(conf.get('database', 'name'))
+        else:
+            db_url = '{0}://{1}:{2}@{3}/{4}'.format(
+                conf.get('database', 'type'),
+                conf.get('database', 'user'),
+                conf.get('database', 'password'),
+                conf.get('database', 'host'),
+                conf.get('database', 'name')
+            )
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = create_engine(db_url)
+    models.Base.metadata.create_all(bind=engine)
 
-api = Api(conf.get('api', 'consumer_key'), conf.get('api', 'consumer_secret'))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    api = Api(conf.get('api', 'consumer_key'), conf.get('api',
+                                                        'consumer_secret'))
+
+if __name__ == '__main__':
+    run()
